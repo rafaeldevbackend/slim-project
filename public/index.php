f5fcb8cbe394e33209c786c189d52bb720844d34
Rafael Rodrigues Alves Teixeira <rafaelrat2019@gmail.com>
2024-11-25 11:30:07 -0300

    Remover configuração setBasePath

--- public/index.php
+++ public/index.php
@@ -39,8 +39,6 @@ AppFactory::setContainer($container);
 $app = AppFactory::create();
 $callableResolver = $app->getCallableResolver();
 
-$app->setBasePath('/slim-project');
-
 // Register middleware
 $middleware = require __DIR__ . '/../app/middleware.php';
 $middleware($app);
